4db3d0154210eb70cffb4dfcdf77ae2cca1d8f7f
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-02 17:52:44 -0500

    Task 2- Sales Report

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,11 @@ if (purchaseAmount > 100) {
     finalAmount = purchaseAmount *0.9; //10% discount
     console.log(`Final amount after discount: $${finalAmount}`); //print using template literal
 };
+
+//Task 2: For Loop - Scenario: Sales Report
+let sales = [120, 85, 200, 150,90]; //array of sales
+let totalSales = 0; 
+for (let i = 0; i < sales.length; i++){
+    totalSales += sales[i]
+}
+console.log(`Total Sales: $${totalSales}`); //template literal
